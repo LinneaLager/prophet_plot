d3f8886692b2dc8439bbe13cf6ce8e73c3393fc4
LinneaLager <89390286+LinneaLager@users.noreply.github.com>
2022-04-02 17:40:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ plt.style.use('fivethirtyeight')
 header = st.beta_container()
 dataset = st.beta_container()
 features = st.beta_container()
-model_training = st.beta_container()
+#model_training = st.beta_container()
 
 
 st.markdown(
