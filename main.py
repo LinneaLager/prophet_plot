4b7d8bdde0784e938840fd6a1051e5256be7c8bf
LinneaLager <89390286+LinneaLager@users.noreply.github.com>
2022-04-02 17:35:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ header = st.beta_container()
 dataset = st.beta_container()
 features = st.beta_container()
 model_training = st.beta_container()
-figure_plot = st.beta_container()
 
 
 st.markdown(
@@ -49,6 +48,7 @@ with dataset:
 	st.header('Covid-19 dataset')
 	st.text('Source can be found here: https://www.kaggle.com/datasets/imdevskp/corona-virus-report')
 
+	covid_data['ds'] = pd.to_datetime(covid_data['ds'])
 	covid_data = get_data('https://raw.githubusercontent.com/LinneaLager/Prophet_Covid_19/main/streamlit.csv')
 	st.write(covid_data.head())
 
@@ -68,22 +68,27 @@ with model_training:
 	st.header('Time to train the model!')
 	st.text('Here you get to choose the hyperparameters of the model and see how the performance changes!')
 
-covid_data['ds'] = pd.to_datetime(covid_data['ds'])
-covid_data.columns = ["ds","y"]
-model = Prophet(growth="linear", seasonality_mode="multiplicative", changepoint_prior_scale=30, seasonality_prior_scale=35, daily_seasonality=False, weekly_seasonality=False, yearly_seasonality=False
+
+	covid_data['ds'] = pd.to_datetime(covid_data['ds'])
+
+	covid_data.columns = ["ds","y"]
+
+	model = Prophet(growth="linear", seasonality_mode="multiplicative", changepoint_prior_scale=30, seasonality_prior_scale=35, daily_seasonality=False, weekly_seasonality=False, yearly_seasonality=False
                 ).add_seasonality(
                     name='montly',
                     period=30,
                     fourier_order=30)
 
-model.fit(covid_data)
 
-future = model.make_future_dataframe(periods= 30, freq='d')
+	model.fit(covid_data)
 
-forecast = model.predict(future)
 
-with figure_plot:
-    chart_data = pd.DataFrame(
-     columns=['ds', 'y'])
+	future = model.make_future_dataframe(periods= 30, freq='d')
 
-    st.line_chart(chart_data)
+
+	forecast = model.predict(future)
+
+	chart_data = pd.DataFrame(
+     
+	columns=['ds', 'y'])
+	st.line_chart(chart_data)
